074192984be60b14fb6d801256f47f83db38fed1
4dealp21 <4dealp21@solent.ac.uk>
2020-10-20 16:14:21 +0100

    Updates

---
+++ precache-manifest.63afa3a322b5ef045a4c87daa18fd3d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd6cbac97f2d3e63c19c89e0e8f95ed",
+    "url": "/interaction-design/index.html"
+  },
+  {
+    "revision": "8129027d828147b83168",
+    "url": "/interaction-design/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce8d43822fe1d8fd2e09",
+    "url": "/interaction-design/static/js/2.bf0fd0d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/interaction-design/static/js/2.bf0fd0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8129027d828147b83168",
+    "url": "/interaction-design/static/js/main.27cb93a6.chunk.js"
+  },
+  {
+    "revision": "2810460e4a2244f3cbbe",
+    "url": "/interaction-design/static/js/runtime-main.ec7e679c.js"
+  },
+  {
+    "revision": "43ef9347d0b8eb5c2d4d9593008b31bd",
+    "url": "/interaction-design/static/media/loading.43ef9347.gif"
+  }
+]);
